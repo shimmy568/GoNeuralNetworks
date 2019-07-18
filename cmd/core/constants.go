d51c5b035ad7af710ef46d60ac52a77c81cc3cf2
Owen Anderson <owenthomasanderson@gmail.com>
2019-07-18 14:55:24 -0400

    got forward pass working

--- cmd/core/constants.go
+++ cmd/core/constants.go
@@ -3,14 +3,11 @@ package core
 // This file just contains the constants used in net.go
 // This is things like dimensions of the network, number of inputs and number of
 
-// InputCount Is the number of inputs into the network
-const InputCount = 3
-
 // InputNodeCount is the number of input nodes in the network
 const InputNodeCount = 1
 
 // HiddenLayerCount is the number of hidden layers in the network
 const HiddenLayerCount = 1
 
-// HiddenLayerSizes is the size of each of the hidden layers
-var HiddenLayerSizes = [1]int{5}
+// HiddenLayerSize is the number of nodes in the hidden layers
+var HiddenLayerSize = 5
--- cmd/main.go
+++ cmd/main.go
@@ -3,65 +3,14 @@ package main
 import (
 	"fmt"
 
-	"gonum.org/v1/gonum/mat"
+	"github.com/shimmy568/NGin/cmd/core"
 )
 
-func matPrint(X mat.Matrix) {
-	fa := mat.Formatted(X, mat.Prefix(""), mat.Squeeze())
-	fmt.Printf("%v\n", fa)
-}
-
 func main() {
-	u := mat.NewVecDense(3, []float64{1, 2, 3})
-	v := mat.NewVecDense(3, []float64{4, 5, 6})
+	net := core.CreateNetwork(3, 10, 1, 5)
 
-	// This doesn’t work
-	// a := u[0]
+	data := []float64{1, 2, 3}
+	o := net.ForwardPass(data)
 
-	// Find 1st element of u (returns value)
-	u.AtVec(1)
-
-	// Equivalent getter method for vectors
-	// The At method may be used on anything that satisfies the Matrix // interface.
-	// (returns value)
-	u.At(1, 0)
-
-	// Overwrite 1st element of u with 33.2
-	u.SetVec(1, 33.2)
-
-	// All vector addition is done in place to reduce overhead
-	w := mat.NewVecDense(3, nil)
-	w.AddVec(u, v) // Add u and v into w
-	// u.AddVec(u, v) // Add u and v into u (to save space)
-	println("u + v: ")
-	matPrint(w)
-
-	// Add scaled vector
-	// u + alpha * v
-	// u + 2v
-	w.AddScaledVec(u, 2, v)
-	println("u + 2 * v: ")
-	matPrint(w)
-
-	// Subtract v from u
-	// v - u
-	w.SubVec(u, v)
-	println("v - u: ")
-	matPrint(w)
-
-	// Scale u by alpha
-	// Just multiply a vector by a scalar
-	w.ScaleVec(23, u)
-	println("u * 23: ")
-	matPrint(w)
-
-	// Compute the dot product of u and v
-	// Since float64’s don’t have a dot method, this is not done
-	//inplace
-	d := mat.Dot(u, v)
-	println("u dot v: ", d)
-
-	// Find length of v
-	l := v.Len()
-	println("Length of v: ", l)
+	fmt.Printf("%v\n", o)
 }
