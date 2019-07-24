6cb922e692f46b0d8724f20cc429a102f5f5c064
Owen Anderson <owenthomasanderson@gmail.com>
2019-07-23 23:16:16 -0400

    pseudo code for main function for ff network

--- cmd/handwirting_ff.go
+++ cmd/handwirting_ff.go
@@ -1,7 +1,17 @@
 package main
 
-// This file is for holding the logic assocated with having
+// This file is for holding the logic associated with having
 
 func runHandwritingFF() {
-	// TODO write pseudo code for running network
+	// TODO Implement pseudo code
+
+	// Get list of images to load from disk
+
+	// Segment data set into a training set and a testing set
+
+	// Load images from disk
+
+	// Train network for the dataset
+
+	// Test network on dataset to check trained accuracy
 }
